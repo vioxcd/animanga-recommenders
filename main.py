58487280af49ddf5319a735c63d7a56abcd0f0d1
RuslanUchan <ruslanuchan97@gmail.com>
2023-03-21 19:04:41 +0700

    add exception when user_id is more than currently sampled users count

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 from src.models import Item, MediaType
 from src.recommendations import get_recommended_items
 
+SAMPLED_USERS_COUNT = 4
+
 
 @app.get("/{user_id}/")
 async def get_recommendations(user_id: int,
                               media_type: MediaType = MediaType.MANGA,
                               k: int = 100) -> list[Item]:
+
+    # as currently the app samples 4 users,
+    # throw error if given `user_id` is equal or more than 4
+    if user_id >= SAMPLED_USERS_COUNT:
+        raise HTTPException(status_code=404,
+                            detail="user_id can only be between 0 to 3")
+
     items = get_recommended_items(user_id, media_type, k)
     return [item.dict() for item in items]
